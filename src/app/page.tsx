0abf475ba8cd062a2b41d49ede16a659116a5d8c
Ala Eddine Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-01 21:07:22 +0100

    landing page prepared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,66 @@
 import Image from "next/image";
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
+import { MarqueeDemo } from "@/components/Marquee";
+import LogoSlider from "@/components/LogoSlider";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="relative min-h-screen overflow-hidden container m-auto">
+      {/* Background */}
+      <div
+        className="absolute inset-0 -z-10 h-screen w-full bg-red-500"
+        style={{
+          backgroundImage: "url('/HeroBackgound.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+  <div className="absolute bottom-0 inset-x-0 h-80 bg-gradient-to-t from-black to-black/0" />
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+      {/* Foreground content */}
+      <div className="flex items-center justify-center h-screen text-white flex-wrap ">
+        <div className="flex flex-wrap justify-center items-center h-[50%] relative top-10">
+        <div>
+
+          
+        <ShimmerButton className="shadow-2xl gap-2">
+          <Image
+            src='/Icon.png' alt="Icon"
+            width={35}
+            height={35}
+          />
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white lg:text-lg">
+            Proudly Serving 40+ Clients
+          </span>
+        </ShimmerButton>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+
+        <div className="flex justify-center flex-wrap gap-8"> 
+          <h1 className="text-7xl w-[80%]  text-center ">Simplifying brands and driving Success through strategic design</h1>
+          <p className="text-xl w-[35%]  text-gray-400  text-center ">We craft bold identities and digital experiences for brands ready to make their mark.</p>
+
+        </div>
+
+
+        <div>
+         <button className="bg-gray-100 text-black p-2 pr-8 pl-8 rounded-md  text-lg font-semibold">
+            Book Free Consultaion
+          </button>
+        </div>
+        
+        </div>
+        
+        <div className="w-full justify-center flex relative bottom-30">
+
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
