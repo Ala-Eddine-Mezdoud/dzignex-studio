94094522e491bc19af9ff678aab0689dbfb40450
Ala Eddine Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-04 23:23:44 +0100

    services and process sections

--- src/components/sections/Process.tsx
+++ src/components/sections/Process.tsx
@@ -1,16 +1,71 @@
 import React from 'react'
-import { ShimmerButton } from '../magicui/shimmer-button'
+import { ShimmerButtonLight } from '../magicui/shimmer-button-light'
+import LightRays from '../LightGRays'
+import Image from 'next/image'
 
 const Process = () => {
   return (
-    <div className='text-white h-screen bg-green-500 flex justify-center'>
-              <div>
-                      <ShimmerButton className="shadow-2xl gap-2">
-                        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white lg:text-lg">
-                          Process
-                        </span>
-                      </ShimmerButton>
-              </div>
+    <div className='relative w-full  flex justify-center  overflow-hidden border rounded-xl border-gray-700 pr-5 pl-5'>
+      
+      {/* Background LightRays */}
+      <div className='absolute inset-0 -z-10'>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#110eff"
+          raysSpeed={1.5}
+          lightSpread={2}
+          rayLength={2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="w-full h-full"
+          fadeDistance={2}
+        />
+      </div>
+
+      {/* Foreground content */}
+ <div className="flex  justify-center  text-white flex-wrap gap-20">
+    <div className="flex flex-col items-center lg:gap-5 gap-15 text-center relative mt-20 w-full">
+      
+      {/* Shimmer Button */}
+      <ShimmerButtonLight className="shadow-2xl gap-2">
+        <Image src="/Icon.png" alt="Icon" width={35} height={35} />
+        <span className="whitespace-pre-wrap text-sm font-medium tracking-tight text-white sm:text-base lg:text-lg">
+          Process
+        </span>
+      </ShimmerButtonLight>
+
+      {/* Heading + Paragraph */}
+      <div className="flex flex-col gap-6 max-w-5xl px-4 items-center ">
+        <h1 className="text-2xl sm:text-4xl lg:text-4xl font-medium lg:w-[55%]">We Drive the Process, You Focus On Business</h1>
+        <p className="text-base sm:text-lg lg:text-xl text-gray-400 max-w-2xl mx-auto">
+          Every project follows a proven path from discovery to delivery ensuring your vision becomes an unforgettable reality.
+        </p>
+      </div>
+
+    </div>
+<div className="w-full">
+
+  {/* Project Cards Wrapper */}
+  <div className="grid gap-5 lg:grid-cols-4 bg-white/10 rounded-lg p-2">
+    {[1, 2, 3, 4].map((i) => (
+      <div
+        key={i}
+        className="bg-white/10 p-2 rounded-xl flex flex-wrap justify-center gap-5 p-4 "
+      >
+        <div className='h-12 w-12 bg-white rounded-full'></div>
+        <h1 className='font-medium text-xl'>Research & Discovery</h1>
+        <p className='text-gray-500 text-center'>Conduct comprehensive market and audience analysis to inform a targeted design strategy.</p>
+      </div>
+    ))}
+  </div>
+
+</div>
+<div className='h-16'></div>
+
+  </div>
+
     </div>
   )
 }
--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,17 +1,91 @@
 import React from 'react'
-import { ShimmerButton } from '../magicui/shimmer-button'
+import { ShimmerButtonLight } from '../magicui/shimmer-button-light'
+import LightRays from '../LightGRays'
+import Image from 'next/image'
 
 const Services = () => {
   return (
-    <div className=' bg-white h-screen flex justify-center'>
+    <div className='relative w-full  flex justify-center  overflow-hidden border rounded-xl border-gray-700 pr-5 pl-5'>
+      
+      {/* Background LightRays */}
+      <div className='absolute inset-0 -z-10'>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#110eff"
+          raysSpeed={1.5}
+          lightSpread={2}
+          rayLength={2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="w-full h-full"
+          fadeDistance={2}
+        />
+      </div>
 
-              <div>
-                      <ShimmerButton className="shadow-2xl gap-2">
-                        <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white lg:text-lg">
-                          Services
-                        </span>
-                      </ShimmerButton>
-              </div>
+      {/* Foreground content */}
+ <div className="flex  justify-center  text-white flex-wrap gap-20">
+    <div className="flex flex-col items-center lg:gap-5 gap-15 text-center relative mt-20 w-full">
+      
+      {/* Shimmer Button */}
+      <ShimmerButtonLight className="shadow-2xl gap-2">
+        <Image src="/Icon.png" alt="Icon" width={35} height={35} />
+        <span className="whitespace-pre-wrap text-sm font-medium tracking-tight text-white sm:text-base lg:text-lg">
+          Services
+        </span>
+      </ShimmerButtonLight>
+
+      {/* Heading + Paragraph */}
+      <div className="flex flex-col gap-6 max-w-5xl px-4 items-center ">
+        <h1 className="text-2xl sm:text-4xl lg:text-4xl font-medium lg:w-[60%]">Expertly Crafted Solutions for Ambitious Brands</h1>
+        <p className="text-base sm:text-lg lg:text-xl text-gray-400 max-w-2xl mx-auto">
+          Powerful visual storytelling crafted to overcome your challenges and accelerate your brand’s success.
+        </p>
+      </div>
+
+    </div>
+<div className="w-full">
+
+  {/* Project Cards Wrapper */}
+  <div className="grid gap-5 lg:grid-cols-3 ">
+    {[1, 2, 3].map((i) => (
+      <div
+        key={i}
+        className="bg-white/10 p-2 rounded-xl flex flex-col gap-5"
+      >
+        {/* Info */}
+        <div className="relative  p-4 flex flex-col gap-5 justify-between items-start text-white rounded-lg">
+          <div className="flex flex-col gap-5 w-full">
+            <div className="flex flex-wrap gap-3">
+              <div className='h-12 w-12 rounded-full bg-white'></div>
+              <h1 className="text-3xl w-full mt-3 border-b-1 pb-4 border-gray-700">Service Name</h1>
+              <p className="text-lg">
+               We build AI-driven websites that adapt to users and automation.
+              </p>
+            </div>
+
+            <div className="w-full h-48 flex flex-wrap gap-5  relative">
+                            <Image
+                              src="/Rectangle 3.png"
+                              fill
+                              alt="project one alt"
+                              className="object-cover rounded-lg"
+                            />
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    ))}
+  </div>
+
+</div>
+<div className='h-16'></div>
+
+  </div>
+
     </div>
   )
 }
