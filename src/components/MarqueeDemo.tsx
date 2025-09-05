d6ec24d39fb4b65649ddce6d94d6f90a8fb62a96
Ala Eddine Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-05 14:51:24 +0100

    Testimonials

--- src/components/MarqueeDemo.tsx
+++ src/components/MarqueeDemo.tsx
@@ -3,21 +3,21 @@ import { Marquee } from "@/components/magicui/marquee";
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
+    name: "Omar.B",
+    username: "Founder, TimePlus",
+    body: "Very satisfied with the experience with Dzignex Studio! Thank you for the fast service, attentive listening to our needs, and the professional quality of communication and execution. Excellent value for money that exceeded our expectations.",
     img: "https://avatar.vercel.sh/jack",
   },
   {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
+    name: "Abdennour.A",
+    username: "Founder, Avure Skincare",
+    body: "We've been working with Dzignex Studio for months and we are truly impressed by your professionalism, responsiveness, and eye for modern, minimalist design. You perfectly captured our vision at Avure and translated it into a strong visual identity. A talented and dedicated partner we highly recommend. Thank you for your great work!",
     img: "https://avatar.vercel.sh/jill",
   },
   {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
+    name: "Abdennour.B",
+    username: "Founder, SLeek Marketing Agency",
+    body: "Honestly, I really liked your work! the logo shows the spirit and identity of my project with a modern touch, the colors, the writing and the design are well chosen and give a professional and creative result. it's exactly the image I wanted for my clients. Thank you for your professionalism and hard work.",
     img: "https://avatar.vercel.sh/john",
   },
   {
@@ -57,14 +57,14 @@ const ReviewCard = ({
   return (
     <figure
       className={cn(
-        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative h-full w-84 cursor-pointer overflow-hidden rounded-xl border p-4 border-gray-700",
         // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        " bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        " dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
       )}
     >
-      <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-row items-center gap-2 border-b-1 border-gray-600 pb-5">
         <img className="rounded-full" width="32" height="32" alt="" src={img} />
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">
@@ -73,7 +73,7 @@ const ReviewCard = ({
           <p className="text-xs font-medium dark:text-white/40">{username}</p>
         </div>
       </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      <blockquote className="mt-2 text-md text-gray-400">{body}</blockquote>
     </figure>
   );
 };
@@ -83,11 +83,6 @@ export function MarqueeDemo() {
     <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
