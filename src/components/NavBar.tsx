7ad39fb3b81c883ca2d04728e6f31912938e6e27
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-07 14:34:08 +0100

    navbar done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,12 +11,16 @@ export default function Navbar() {
     <nav
       className="
         sticky top-0 z-50 w-full 
-        bg-white/10 backdrop-blur-2xl 
-        border-b border-white/5
+        backdrop-blur-2xl 
+        border-b border-white/10
         text-white
+        flex justify-center items-center
+        h-[88px]
+        shadow-[0_5px_20px_0_#01011033]
+        bg-[#0C3EFF]/10
       "
     >
-      <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-3">
+      <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-3 w-full">
         {/* Logo */}
         <span className="font-bold text-xl">
                         <Link href="/" className="hover:underline">
@@ -25,32 +29,32 @@ export default function Navbar() {
           </span>
 
         {/* Desktop Menu */}
-        <ul className="hidden sm:flex gap-6 list-none m-0 p-0">
+        <ul className="hidden sm:flex gap-8 list-none m-0 p-0 text-xl font-semibold">
           <li>
-              <Link href="/" className="hover:underline">
+              <Link href="/" className="hover:border-b border-[#0c3eff] pb-1 text-[#0c3eff]">
                 Home
               </Link>
           </li>
           <li>
-              <Link href="/about" className="hover:underline">
-                About
+              <Link href="/about" className="hover:border-b pb-1">
+                About Us
               </Link>
           </li>
           <li>
-            <Link  href="/projects">
+            <Link  href="/projects" className="hover:border-b pb-1">
             Projects
             </Link>
           </li>
           <li>
-            <Link  href="/contact" className="bg-blue-500 px-3 py-2 rounded-lg text-white hover:bg-blue-600 transition">
-              Contact
+            <Link  href="/contact" className="bg-[#0c3eff] pt-2 pb-2  pl-6 pr-6 rounded-lg text-white hover:bg-blue-600 transition ml-20">
+              Contact Us
             </Link>
           </li>
         </ul>
 
         {/* Mobile Hamburger */}
         <button
-          className="sm:hidden"
+          className="sm:hidden "
           onClick={() => setOpen(!open)}
           aria-label="Toggle menu"
         >
