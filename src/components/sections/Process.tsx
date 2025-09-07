989f3c91a4a05cb07f6256e3f2f8c56ef2b0042f
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-07 18:41:59 +0100

    latest

--- src/components/sections/Process.tsx
+++ src/components/sections/Process.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ShimmerButtonLight } from '../magicui/shimmer-button-light'
 import Image from 'next/image'
+import { ShimmerButton } from '../magicui/shimmer-button'
 
 const Process = () => {
   return (
@@ -9,37 +10,41 @@ const Process = () => {
 
       {/* Foreground content */}
  <div className="flex  justify-center  text-white flex-wrap gap-20">
-    <div className="flex flex-col items-center lg:gap-5 gap-15 text-center relative mt-20 w-full">
-      
-      {/* Shimmer Button */}
-      <ShimmerButtonLight className="shadow-2xl gap-2">
-        <Image src="/Icon.png" alt="Icon" width={35} height={35} />
+      <div className="flex justify-center items-center flex-wrap gap-[40px] text-center">
+
+        <div className='w-full flex flex-wrap justify-center mt-[128px]'>
+
+      <ShimmerButton className="shadow-2xl gap-2">
+        <Image src="/projects.svg" alt="Icon" width={36} height={36} />
         <span className="whitespace-pre-wrap text-sm font-medium tracking-tight text-white sm:text-base lg:text-lg">
-          Process
+          Projects
         </span>
-      </ShimmerButtonLight>
+      </ShimmerButton>
+        </div>
 
-      {/* Heading + Paragraph */}
-      <div className="flex flex-col gap-6 max-w-5xl px-4 items-center ">
-        <h1 className="text-2xl sm:text-4xl lg:text-4xl font-medium lg:w-[55%]">We Drive the Process, You Focus On Business</h1>
-        <p className="text-base sm:text-lg lg:text-xl text-gray-400 max-w-2xl mx-auto">
+        <div className='w-full flex justify-center'>
+        <h1 className="text-white text-[35px] sm:text-[40px] tracking-[-2px] leading-[42px] font-medium  sm:w-[35%]">
+          We Drive the Process, You Focus On Business
+        </h1>
+        </div>
+
+       <p className="text-base sm:text-lg lg:text-[16px] text-[#f3f6ff]/60 max-w-2xl mx-auto sm:w-[40%] text-[15px] -mt-[20px]">
           Every project follows a proven path from discovery to delivery ensuring your vision becomes an unforgettable reality.
-        </p>
-      </div>
+      </p>
 
-    </div>
+      </div>
 <div className="w-full">
 
   {/* Project Cards Wrapper */}
-  <div className="grid gap-5 lg:grid-cols-4 bg-white/10 rounded-lg p-2">
+  <div className="grid gap-5 lg:grid-cols-4 shadow-[0_5px_20px_0_#01011033] bg-black  backdrop-blur-2xl border border-[#f3f6ff]/15   p-[20px] rounded-[40px]">
     {[1, 2, 3, 4].map((i) => (
       <div
         key={i}
-        className="bg-white/10 p-2 rounded-xl flex flex-wrap justify-center gap-5 p-4 "
+        className="bg-black border border-[#f3f6ff]/15 p-2 rounded-[20px] flex flex-wrap justify-center gap-5 p-4 "
       >
-        <div className='h-12 w-12 bg-white rounded-full'></div>
-        <h1 className='font-medium text-xl'>Research & Discovery</h1>
-        <p className='text-gray-500 text-center'>Conduct comprehensive market and audience analysis to inform a targeted design strategy.</p>
+        <div className='h-[64px] w-[64px] bg-white rounded-full'></div>
+        <h1 className='font-medium text-[23px] tracking-[-1x]' >Research & Discovery</h1>
+        <p className='text-[#f3f6ff]/60 text-center'>Conduct comprehensive market and audience analysis to inform a targeted design strategy.</p>
       </div>
     ))}
   </div>
--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -19,18 +19,27 @@ const Services = () => {
       {/* Foreground content */}
  <div className="flex  justify-center px-[32px] pb-[40px] text-white flex-wrap gap-20">
       <div className="flex justify-center items-center flex-wrap gap-[40px] text-center">
-        <div className='w-full flex justify-center mt-[128px]'>
+
+        <div className='w-full flex flex-wrap justify-center mt-[128px]'>
 
       <ShimmerButton className="shadow-2xl gap-2">
         <Image src="/projects.svg" alt="Icon" width={36} height={36} />
         <span className="whitespace-pre-wrap text-sm font-medium tracking-tight text-white sm:text-base lg:text-lg">
-          Projects
+          Services
         </span>
       </ShimmerButton>
         </div>
-        <h1 className="text-white text-[35px] sm:text-[40px] tracking-[-2px] leading-[42px] font-medium  sm:w-[50%]">
-          Discover The Impact Behind Every Brand
+
+        <div className='w-full flex justify-center'>
+        <h1 className="text-white text-[35px] sm:text-[40px] tracking-[-2px] leading-[42px] font-medium  sm:w-[40%]">
+          Expertly Crafted Solutions for Ambitious Brands
         </h1>
+        </div>
+
+       <p className="text-base sm:text-lg lg:text-[16px] text-[#f3f6ff]/60 max-w-2xl mx-auto sm:w-[40%] text-[15px] -mt-[20px]">
+          Powerful visual storytelling crafted to overcome your challenges and accelerate your brand’s success.
+      </p>
+
       </div>
 <div className="">
 
@@ -43,22 +52,17 @@ const Services = () => {
       >
         {/* Info */}
         <div className="relative  p-4 flex flex-col gap-5 justify-between items-start text-white rounded-lg">
-          <div className="flex flex-col gap-5 w-full">
-            <div className="flex flex-wrap gap-3">
-              <div className='h-12 w-12 rounded-full bg-white'></div>
-              <h1 className="text-3xl w-full mt-3 border-b-1 pb-4 border-gray-700">Service Name</h1>
-              <p className="text-lg">
+          <div className="flex flex-col gap-[28px] w-full">
+            <div className="flex flex-wrap gap-[40px]">
+              <div className='h-[52px] w-[52px] rounded-full bg-white'></div> {/* circle */}
+              <h1 className="text-[24px] font-medium leading-[24px] tracking-[-1px] w-full  border-b-1 pb-[24px] border-gray-700">Service Name</h1>
+              <p className="text-[16px] text-[#F3F6FF]/60 leading-[22px] tracking-[-1px] -mt-[20px]">
                We build AI-driven websites that adapt to users and automation.
               </p>
             </div>
 
-            <div className="w-full h-48 flex flex-wrap gap-5  relative">
-                            <Image
-                              src="/Rectangle 3.png"
-                              fill
-                              alt="project one alt"
-                              className="object-cover rounded-lg"
-                            />
+            <div className="w-full h-48 flex flex-wrap gap-5 bg-blue-500 rounded-[12px] relative">
+ 
             </div>
           </div>
 
