98a481a6d5d105d2b0eee903567b483b6e672de1
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-07 14:38:49 +0100

    font size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,10 +29,10 @@ export default function Navbar() {
           </span>
 
         {/* Desktop Menu */}
-        <ul className="hidden sm:flex gap-8 list-none m-0 p-0 text-[18px] font-semibold">
+        <ul className="hidden sm:flex gap-8 list-none m-0 p-0 text-xl font-semibold">
           <li>
               <Link href="/" className="hover:border-b border-[#0c3eff] pb-1 text-[#0c3eff] ">
-                Home
+                Homes
               </Link>
           </li>
           <li>
