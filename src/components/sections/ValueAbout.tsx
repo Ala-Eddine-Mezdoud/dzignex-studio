b2a214a80e953af785840bb4987fc1b5c225b69e
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-09 15:15:53 +0100

    Values

--- src/components/sections/ValueAbout.tsx
+++ src/components/sections/ValueAbout.tsx
@@ -13,7 +13,7 @@ const ValueAbout = () => {
             <div className='flex justify-between  flex-wrap gap-[20px] w-full bg-[#0C3EFF]/10 p-[20px] border border-[#f3f6ff]/10 rounded-[20px]'>
                 <div className='flex gap-[24px] sm:gap-[40px] items-center '>
                     <Image
-                      src="/contactIcon.svg"
+                      src="/glass icon.svg"
                       alt="value1"
                       width={92}
                       height={92}
@@ -28,13 +28,13 @@ const ValueAbout = () => {
             <div className='flex justify-between  flex-wrap gap-[20px] w-full bg-[#0C3EFF]/10 p-[20px] border border-[#f3f6ff]/10 rounded-[20px]'>
                 <div className='flex gap-[24px] sm:gap-[40px] items-center '>
                     <Image
-                      src="/contactIcon.svg"
+                      src="/glass icon1.svg"
                       alt="value1"
                       width={92}
                       height={92}
                       className="w-[64px] h-[64px] sm:w-[92px] sm:h-[92px]"
                     />
-                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Specialized \nExpertise"}</p>
+                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Client \nSatisfaction"}</p>
                 </div>
                 <div  className='sm:whitespace-pre-line  text-[16px] sm:text-[18px] w-full sm:w-auto text-[#f3f6ff]/80'>
                     {"Our three co-founders bring senior-level skills across \nBranding, Packaging, and UI/UX. Diverse backgrounds merge \ninto one unified creative vision."}
@@ -43,13 +43,13 @@ const ValueAbout = () => {
             <div className='flex justify-between  flex-wrap gap-[20px] w-full bg-[#0C3EFF]/10 p-[20px] border border-[#f3f6ff]/10 rounded-[20px]'>
                 <div className='flex gap-[24px] sm:gap-[40px] items-center '>
                     <Image
-                      src="/contactIcon.svg"
+                      src="/glass icon2.svg"
                       alt="value1"
                       width={92}
                       height={92}
                       className="w-[64px] h-[64px] sm:w-[92px] sm:h-[92px]"
                     />
-                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Specialized \nExpertise"}</p>
+                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Long-Term \nRelationships"}</p>
                 </div>
                 <div  className='sm:whitespace-pre-line  text-[16px] sm:text-[18px] w-full sm:w-auto text-[#f3f6ff]/80'>
                     {"Our three co-founders bring senior-level skills across \nBranding, Packaging, and UI/UX. Diverse backgrounds merge \ninto one unified creative vision."}
@@ -58,13 +58,13 @@ const ValueAbout = () => {
             <div className='flex justify-between  flex-wrap gap-[20px] w-full bg-[#0C3EFF]/10 p-[20px] border border-[#f3f6ff]/10 rounded-[20px]'>
                 <div className='flex gap-[24px] sm:gap-[40px] items-center '>
                     <Image
-                      src="/contactIcon.svg"
+                      src="/glass icon3.svg"
                       alt="value1"
                       width={92}
                       height={92}
                       className="w-[64px] h-[64px] sm:w-[92px] sm:h-[92px]"
                     />
-                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Specialized \nExpertise"}</p>
+                    <p className='sm:text-[24px] text-[28px] whitespace-pre-line font-medium tracking-[-1px] leading-[30px]'>{"Collaboration \nDrive Us"}</p>
                 </div>
                 <div  className='sm:whitespace-pre-line  text-[16px] sm:text-[18px] w-full sm:w-auto text-[#f3f6ff]/80'>
                     {"Our three co-founders bring senior-level skills across \nBranding, Packaging, and UI/UX. Diverse backgrounds merge \ninto one unified creative vision."}
