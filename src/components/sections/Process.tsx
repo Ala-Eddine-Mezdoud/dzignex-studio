a47d3a21b46fcecc0362773605c8c862ab6537bc
Ala-Eddine-Mezdoud <mezdoud.ala.e@gmail.com>
2025-09-22 09:42:15 +0100

    about us bg

--- src/components/sections/Process.tsx
+++ src/components/sections/Process.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { ShimmerButton } from '../magicui/shimmer-button'
-import MagicBento from '../MagicBento'
+
 
 const Process = () => {
   return (
